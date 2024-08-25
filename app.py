ea7a7b70e7bb777b1317e7d76e86f698b7cf9fd8
Jayant-stack <jayanthalapati@gmail.com>
2024-08-24 21:36:02 -0400

    second commit of the code

--- app.py
+++ app.py
@@ -1,12 +1,36 @@
-from flask import Flask
+from flask import Flask,render_template,request,redirect,url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
 
+with app.app_context():
+    db.create_all()
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():
+    tasks = Task.query.all()# put application's code here
+    return render_template('index.html',tasks=tasks)
 
+@app.route('/add', methods=['GET','POST'])
+def add_task():
+    task = request.form.get('task')
+    if task:
+        new_task = Task(content=task)
+        db.session.add(new_task)
+        db.session.commit()
+    return redirect(url_for('index'))
+@app.route('/delete/<int:id>', methods=['GET','POST'])
+def delete_task(id):
+    task_to_delete = Task.query.get_or_404(id)
+    db.session.delete(task_to_delete)
+    db.session.commit()
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run()
